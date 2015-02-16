a67dd5e0f65118e0a6df4f7af6f65f07a3473912
Rupak Ganguly <rupakg@gmail.com>
2015-02-16 18:55:46 -0500

    Add search to main page.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,9 +26,9 @@ module.exports = function(config) {
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-filter/dist/angular-filter.js',
       'bower_components/ng-lodash/build/ng-lodash.js',
       'bower_components/ngDialog/js/ngDialog.js',
-      'bower_components/angular-filter/dist/angular-filter.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
