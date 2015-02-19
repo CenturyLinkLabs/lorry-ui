eb7700f382488b77040fafa69e8f70286cd784f8
Rupak Ganguly <rupakg@gmail.com>
2015-02-19 15:50:02 -0500

    Refactor service definition builder to use the transformer service.

--- app/scripts/controllers/document.js
+++ app/scripts/controllers/document.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('lorryApp').controller('DocumentCtrl', ['$scope', '$log', 'yamlValidator', 'lodash',
-  function ($scope, $log, yamlValidator, lodash) {
+angular.module('lorryApp').controller('DocumentCtrl', ['$scope', '$log', 'yamlValidator', 'serviceDefTransformer',
+  function ($scope, $log, yamlValidator, serviceDefTransformer) {
 
     $scope.yamlDocument = {};
 
@@ -22,24 +22,7 @@ angular.module('lorryApp').controller('DocumentCtrl', ['$scope', '$log', 'yamlVa
     };
 
     $scope.$watchGroup(['yamlDocument.lines', 'yamlDocument.errors'], function(){
-      var currentCollection;
-      $scope.serviceDefinitions = [];
-
-      lodash.forEach($scope.yamlDocument.lines, function(line, index){
-        var lineNumber = index + 1;
-        var lineDetails = {
-          text: line,
-          lineNumber: lineNumber,
-          errors: lodash.select($scope.yamlDocument.errors, { error: { line: lineNumber } })
-        };
-
-        if (lodash.startsWith(line, ' ')) {
-          currentCollection.push(lineDetails);
-        } else {
-          currentCollection = [lineDetails];
-          $scope.serviceDefinitions.push(currentCollection);
-        }
-      });
+      $scope.serviceDefinitions = serviceDefTransformer.fromYamlDocument($scope.yamlDocument);
     });
 
   }]);
