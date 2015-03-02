f3f7103ddf40e19767b8db04b226f61345bc8617
Pat Cox <patrick.cox@savvis.com>
2015-03-02 12:19:40 -0600

    Files for CircleCI and QA

---
+++ bin/deploy_qa.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh root@8.22.9.40 "docker rm -f lorry_ui; docker rmi -f centurylink/lorry-ui:qa; docker pull centurylink/lorry-ui:qa; docker run -d --name lorry_ui -p 8080:80 centurylink/lorry-ui:qa"
