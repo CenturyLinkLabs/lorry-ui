cd3b3d7a2c66acb62a059ca7d5c8d8e8c9586aaa
Mike Arnold <dharmamike@gmail.com>
2015-04-13 13:25:25 -0500

    refactoring service definition display directive to use link rather than controller

--- app/scripts/directives/service-definition-display.js
+++ app/scripts/directives/service-definition-display.js
@@ -1,31 +1,28 @@
 'use strict';
 
 angular.module('lorryApp')
-  .directive('serviceDefinitionDisplay', function ($log, lodash) {
+  .directive('serviceDefinitionDisplay', ['lodash', function (lodash) {
     return {
       scope: {
         serviceDefinition: '='
       },
       restrict: 'E',
       replace: 'true',
-      link: function postLink(scope, element, attrs) {
-        //$log.log(scope.serviceDefinition);
-      },
-      templateUrl: '/scripts/directives/service-definition-display.html',
-      controller: function ($scope) {
-        $scope.hasLines = function () {
-          if (lodash.any($scope.serviceDefinition)) {
-            return !(/^(?:\s|-)/i.test($scope.serviceDefinition[0].text));
+      link: function postLink(scope) {
+        scope.hasLines = function () {
+          if (lodash.any(scope.serviceDefinition)) {
+            return !(/^(?:\s|-)/i.test(scope.serviceDefinition[0].text));
           }
         };
 
-        $scope.classes = function () {
-          return $scope.hasLines() && !$scope.$parent.inEditMode() && !$scope.$parent.inNewServiceMode() ? 'highlightable' : 'disabled';
+        scope.classes = function () {
+          return scope.hasLines() && !scope.$parent.inEditMode() && !scope.$parent.inNewServiceMode() ? 'highlightable' : 'disabled';
         };
 
-        $scope.serviceName = function () {
-          return $scope.serviceDefinition[0].text.split(':')[0]
+        scope.serviceName = function () {
+          return scope.serviceDefinition[0].text.split(':')[0]
         };
-      }
+      },
+      templateUrl: '/scripts/directives/service-definition-display.html'
     };
-  });
+  }]);
