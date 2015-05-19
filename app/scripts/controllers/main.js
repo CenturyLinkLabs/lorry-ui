4deaeb30096e3b61130f90824d8c1209ecf9ca7a
Alex Welch <me@alexwelch.com>
2015-05-19 08:27:33 -0600

    Display a splash page for mobile users [#94499228]

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,9 +5,9 @@
     .module('lorryApp')
     .controller('MainCtrl', MainCtrl);
 
-  MainCtrl.$inject = ['$scope', 'viewHelpers'];
+  MainCtrl.$inject = ['$scope', 'viewHelpers', 'userAgent'];
 
-  function MainCtrl($scope, viewHelpers) {
+  function MainCtrl($scope, viewHelpers, userAgent) {
 
     $scope.swoosh = function () {
       viewHelpers.animateLogo();
@@ -17,6 +17,25 @@
       $scope.$broadcast('document.reset');
     };
 
+    $scope.isMobile = function() {
+      return userAgent.isMobile();
+    };
+
+    $scope.showMobileWarning = function() {
+      if ($scope.forceWebView) {
+        return false;
+      } else {
+        return $scope.isMobile();
+      }
+    };
+
+    $scope.dismissMobileView = function() {
+      $scope.forceWebView = true;
+    };
+
+    $scope.bodyClasses = function() {
+      return $scope.isMobile() ? 'mobile' : null;
+    };
   }
 
 })();
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,16 +6,17 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('lorryApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    userAgent;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _viewHelpers_, _userAgent_) {
     scope = $rootScope.$new();
+    userAgent = _userAgent_;
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
-
 
   describe('$scope.resetWorkspace', function () {
     it ('broadcasts the document.reset event', function () {
@@ -25,4 +26,58 @@ describe('Controller: MainCtrl', function () {
     });
   });
 
+  describe('$scope.isMobile', function () {
+    it('returns false if the userAgent says it is falsy', function() {
+      spyOn(userAgent, 'isMobile').and.returnValue(false);
+      expect(scope.isMobile()).toEqual(false);
+    });
+
+    it('returns true if it is true on the userAgent service', function() {
+      spyOn(userAgent, 'isMobile').and.returnValue(true);
+      expect(scope.isMobile()).toEqual(true);
+    });
+  });
+
+  describe('$scope.bodyClasses', function () {
+    it('returns nothing by default', function() {
+      spyOn(userAgent, 'isMobile').and.returnValue(false);
+      expect(scope.bodyClasses()).toBeNull();
+    });
+
+    it('returns mobile when the on a mobile device', function() {
+      spyOn(userAgent, 'isMobile').and.returnValue(true);
+      expect(scope.bodyClasses()).toContain('mobile');
+    });
+  });
+
+  describe('dismissMobileView', function() {
+    it('sets the forceWebView flag to true', function() {
+      expect(scope.forceWebView).toBeFalsy();
+      scope.dismissMobileView();
+      expect(scope.forceWebView).toEqual(true);
+    });
+  });
+
+  describe('showMobileWarning', function() {
+    it('never shows after dismissMobileView has been called', function() {
+      scope.dismissMobileView();
+      expect(scope.showMobileWarning()).toEqual(false);
+    });
+
+    describe('when forceWebView is falsy, i.e. dismissMobile has not been called', function() {
+      beforeEach(function() {
+        scope.forceWebView = false;
+      });
+
+      it('returns false if the userAgent says it is falsy', function() {
+        spyOn(userAgent, 'isMobile').and.returnValue(false);
+        expect(scope.showMobileWarning()).toEqual(false);
+      });
+
+      it('returns true if it is true on the userAgent service', function() {
+        spyOn(userAgent, 'isMobile').and.returnValue(true);
+        expect(scope.showMobileWarning()).toEqual(true);
+      });
+    });
+  });
 });
