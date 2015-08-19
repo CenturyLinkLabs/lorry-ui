850b8a489fc060548a4c8b797cf787fbab3ca867
Rupak Ganguly <rupakg@gmail.com>
2015-08-19 16:14:37 -0400

    Add new key cpuset.

--- app/scripts/directives/service-definition-edit.js
+++ app/scripts/directives/service-definition-edit.js
@@ -330,7 +330,7 @@
         };
 
         var isKeyTypeString = function (skey) {
-          var stringKeys = ['image', 'build', 'net', 'pid', 'working_dir', 'entrypoint', 'user', 'hostname', 'domainname', 'mac_address', 'mem_limit', 'memswap_limit', 'privileged', 'restart', 'stdin_open', 'tty', 'cpu_shares'];
+          var stringKeys = ['image', 'build', 'net', 'pid', 'working_dir', 'entrypoint', 'user', 'hostname', 'domainname', 'mac_address', 'mem_limit', 'memswap_limit', 'privileged', 'restart', 'stdin_open', 'tty', 'cpu_shares', 'cpuset'];
           return lodash.includes(stringKeys, skey);
         };
 
